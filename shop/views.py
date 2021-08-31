f60c8679d1e1c47d6c92b62cb0768de2c9f5543b
Korobeynikov A <korobeynikov2009@yandex.ru>
2021-08-31 11:40:13 +0300

    Добавлены опции для обновления бд, рефакторинг

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 import os
-from shop.services import add_item_specs, create_categores, create_items, create_specs, set_analogs_and_related, update_prices
+from shop.services import send_to_base
 
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
@@ -15,6 +15,8 @@ from django.contrib import messages
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.conf import settings
+from django.db.models import Count
+
 
 from .forms import CartAddItemForm
 from .cart import Cart
@@ -22,7 +24,7 @@ from .models import Category, Item, Favorite, ItemRating, SpecItem, SpecValue, U
 from account.models import Profile
 from typing import *
 from decimal import Decimal
-from .utils import Group, download_image, get_xml_parser, get_xml_parser2
+from .utils import get_parser
 
 
 
@@ -44,74 +46,78 @@ class ItemsList(ListView):
     paginate_by = 12
     context_object_name = 'items'
     queryset = model.objects.all()
-    #.annotate(avg_review=models.Avg('item_rating__rating')).order_by('-avg_review')
+    # .annotate(avg_review=models.Avg('item_rating__rating')).order_by('-avg_review')
 
     def get_queryset(self):
+
+        qs = super().get_queryset()
+        # qs = qs.prefetch_related('discount').prefetch_related("favorite")
+        qs = qs.annotate(Count("favorite"))
         if 'search' in self.request.GET:
             search_query = self.request.GET.get('search', False)
-            return Item.objects.all().filter(
+            return qs.filter(
                 Q(title__icontains=search_query) |
                 Q(description__icontains=search_query) |
-                Q(category__title=search_query),
+                Q(category__code=search_query),
             )
             #.annotate(avg_review=models.Avg('item_rating__rating')).order_by('-avg_review')
         elif 'cat_code' in self.request.GET:
             category = self.request.GET.get('cat_code', False)
-            print(category)
-            qs = Item.objects.all().filter(category__code=category
-                )
+
+            # print(category)
+            qs = qs.filter(category__code=category)
                 #.annotate(avg_review=models.Avg('item_rating__rating')).order_by('-avg_review')
             if qs:
                 return qs
-            qs = Item.objects.filter(category__parent__code=category)
+            qs = qs.filter(category__parent__code=category)
             return qs
-
-        return super().get_queryset()
+            print(qs)
+        return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context = dict()
-        search_query = self.request.GET.get('search', False)
-
-        # if search_query:
-        #     items = Item.objects.all().filter(
-        #         Q(title__icontains=search_query) |
-        #         Q(description__icontains=search_query) |
-        #         Q(category__title=search_query),
-        #     ).annotate(avg_review=models.Avg('item_rating__rating')).order_by(
-        #         '-avg_review')
-        #     context['items'] = items
-        #     context['is_paginated'] = False
-            
         context['section'] = 'home_page'
         context['nodes'] = Category.objects.all()
-        # print(context)
+        context['favorite'] = Favorite.objects.filter(user=self.request.user).values_list('item_id', flat=True)
         return context
 
 
 class FavoritesItems(ListView, LoginRequiredMixin):
     model = Item
     context_object_name = 'items'
-    
+    extra_context = dict(section='favorites')
+    paginate_by = 12
+ 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(favorite__user=self.request.user)
+        return qs
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        items = Item.objects.all().annotate(avg_review=models.Avg('item_rating__rating')).order_by(
-        '-avg_review').filter(favorite__user=self.request.user)
-        context.update(dict(items=items, section='favorites'))
+        # items = Item.objects.filter(favorite__user=self.request.user)
+        # all().annotate(avg_review=models.Avg('item_rating__rating')).order_by('-avg_review').
         return context
 
 
 class ItemDetail(DetailView):
     model = Item
+    slug_field = 'slug'
+    extra_context = dict(
+        section='detail',
+    )
+
     # template = 'shop/item_detail.html'
     # context_object_name = 'item'
+
+    def get_queryset(self):
+        qs = super().get_queryset().prefetch_related('analogs').prefetch_related('related')
+        return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['comments'] = ItemRating.objects.filter(item=context['object'])
         context['specs'] = SpecValue.objects.filter(item=context['object']).select_related('spec_item')
-        context['section'] = 'detail'
-        print(context)
         return context
 
 
@@ -124,14 +130,21 @@ class CategoryList(View):
         cat = get_object_or_404(Category, code=kwargs['code'])
         chield_categories = cat.get_descendants(include_self=True)
         nodes = Category.objects.filter(parent__in=chield_categories)
-        print(nodes)
+        if nodes:
+            items = Item.objects.filter(category__in=nodes)
+        else:
+            nodes = Category.objects.all()
+            items = Item.objects.filter(category=cat)
         context = dict(
             cat=cat,
-            nodes=nodes
+            nodes=nodes,
+            items=items,
+            title=cat.title
             )
         return render(request=request, template_name=self.template, context=context)
 
 # Функционал добавление и удаления из избранного
+@login_required
 def add_to_favorites(request):
     if request.method == 'POST':
         add_data = {
@@ -159,7 +172,7 @@ def add_to_favorites(request):
     return redirect(request.POST.get('url_from'))
 
 
-@login_required()
+@login_required
 def remove_from_favorites(request):
     if request.method == 'POST':
         id = request.POST.get('id')
@@ -182,7 +195,6 @@ def remove_from_favorites(request):
     return redirect(request.POST.get('url_from'))
 
 
-@login_required()
 def favorites_api(request):
     # all_favorites = Favorite.objects.all()
     if request.user.is_authenticated and request.user.is_active:
@@ -206,7 +218,7 @@ def favorites_api(request):
     return JsonResponse(data, safe=False)
 
 
-@login_required()
+@login_required
 @require_POST
 def add_to_cart(request):
     cart = Cart(request)
@@ -268,7 +280,7 @@ def cart(request):
     return render(request, 'shop/cart.html', context=con)
 
 
-@login_required()
+@login_required
 @require_POST
 def remove_from_cart(request):
     if request.method == 'POST':
@@ -329,29 +341,19 @@ class UpdateDb(View):
     
     def post(self, request, *args, **kwargs):
         data_file = request.FILES.get('items_db', False)
+        print(request.POST)
         if not data_file:
             return redirect('home')
         path = default_storage.save(f"tmp/{data_file}", ContentFile(data_file.read()))
         file_name = os.path.join(settings.MEDIA_ROOT, path)
-        type_xml, parser = get_xml_parser2(file_name)
-        print(type_xml, parser)
-        if type_xml is None or parser is None:
+        file_type, parser = get_parser(file_name)
+        if file_type is None or parser is None:
             messages.error(request, 'Ошибка загрузки - возможно не верный файл')
             return redirect('home')
-
         parser.run()
         result = parser.get_result()
-        if type_xml == 'PRODAT':
-            # result = parser.get_result()
-            groups = result['groups']
-            features = result['features']
-            create_categores(groups)
-            create_specs(features)
-            create_items(result['items'])
-            add_item_specs(result['items'])
-            set_analogs_and_related(result['items'])
-        elif type_xml == 'PRICAT':
-            # result = parser.get_items()
-            update_prices(result['prices'])
-        return redirect('home')        
+        send_to_base(file_type, result, request.POST) 
+        return redirect('home') 
+
+    
     
