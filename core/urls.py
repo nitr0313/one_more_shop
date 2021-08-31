dc21d153f9048bee45b38eb2074187092a5f17bb
Korobeynikov A <korobeynikov2009@yandex.ru>
2021-08-31 11:42:35 +0300

    Обновлен для использования toolbar

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+    except ModuleNotFoundError:
+        pass
